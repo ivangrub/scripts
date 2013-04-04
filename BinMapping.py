ac81202eb63d13d1e4942178eed01e28bd1396c1
Ivan Grubisic <ivan.grub@gmail.com>
2013-04-03 17:20:35 -0700

    Trying to add trimming of the BAM file

--- BinMapping.py
+++ BinMapping.py
@@ -4,7 +4,7 @@ import pysam as pys
 import argparse
 import os
 
-def gen2bin(read,chrlist,headers,bin,chrt):
+def gen2bin(read,chrlist,headers,bin,chrt,headlist):
 	s = read.rname
 	chrom = chrt[s]
 	ind = int(round(read.pos/float(bin-1)))
@@ -20,19 +20,43 @@ def gen2bin(read,chrlist,headers,bin,chrt):
 	off = read.pos - int(x[2])+1
 	k = 1
 	while chrom != x[1] or off < 0:
-		#print read.pos,x
 		x = headers[y-k]['SN'].split('!')
 		off = read.pos - int(x[2]) + 1
 		k += 1
 
-	if off > int(headers[y-k]['LN']) or off < 0:
+	if off > int(headers[y-k+1]['LN']) or off < 0:
 		print 'ERROR',read
 
 	read.pos = off
 	read.tid = y-k+1
-	return read	
-	
-	
+	indice = [-1,0,1]
+	for ran in indice:
+		try:
+			headlist.append(headers[y-k+1+ran]['SN'])
+		except IndexError:
+			continue
+
+	return read,headlist	
+
+def MakeHeader(binheader,binsize):
+	ref = []
+	header = {}
+	header['HD'] = {'VN':'1.0'}
+	for i in binheader:
+		ref.append({'LN':binsize, 'SN':i})
+	header['SQ'] = ref
+	return header	
+
+def TrimBAM(conv,oheader,nheader):
+	trimmed = pys.Samfile('%s_trimmed.bam' % args.o,'wb',template=nheader)
+	for read in conv:
+		binid = oheader[read.tid]
+		index = nheader.index(binid)
+		print read.tid,index
+		read.tid = index
+		trimmed.write(read)
+	trimmed.close()
+
 def head2chr(header):
 	chr = {}
 	i = 0
@@ -92,18 +116,27 @@ chr_tuple = infile.references
 chrindex = head2chr(headerlist)
 binning = offset(headerlist)
 
+binheader = []
 for line in infile:
 	if line.is_unmapped:
 		continue
 	if args.r is '-':
 		outbam.write(line)
-	readmod = gen2bin(line,chrindex,headerlist,binning,chr_tuple)
+	readmod,binheader = gen2bin(line,chrindex,headerlist,binning,chr_tuple,binheader)
 	convbam.write(readmod)
+
+convbam.close()
+conv.close()
+conv = pys.Samfile('%s_converted.bam' % args.o,'rb')
+newheader = MakeHeader(binheader,binning)
+print newheader
+TrimBAM(conv,headerlist,newheader)
+conv.close()
 
 if args.r is '-':
 	outbam.close()
-convbam.close()
+
 infile.close()
-conv.close()
+
 
 
