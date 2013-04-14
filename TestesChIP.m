a7fdd377e5f804d359d7adce48dbfeda80123786
Ivan Grubisic <ivan.grub@gmail.com>
2013-04-13 23:24:49 -0700

    Added code for Tommy's table

--- TestesChIP.m
+++ TestesChIP.m
@@ -40,22 +40,58 @@ for i = 1:length(IP)
 	fid = fopen(sprintf('%s.filtered.peaks.bed',IP{i}));
 	peak = textscan(fid,'%s%d%d','delimiter','\t');
 	fclose(fid);clear fid;
+
 	genesect = zeros(length(peak),length(IP)+4);
 	for j = 1:length(peak)
+		coordind = ceil(ind*50);
+		chromind = peak(j,1) == known(:,1);
+		chrknown = known(chromind,:)
+		midpoint = mean(peak(2),peak(3));
+		promoter = midpoint >= chrknown(:,2)-500 | midpoint <= chrknown(:,2)+500;
+		proximal = midpoint >= chrknown(:,2)-10000 | midpoint <= chrknown(:,2)+10000;
+		distal = midpoint >= chrknown(:,2)-50000 | midpoint <= chrknown(:,2)+50000;
+		if sum(promoter) > 0
+			region = 'promoter';
+		else if sum(proximal) > 0
+			region = 'proximal';
+		else if sum(proximal) > 0
+			region = 'distal';
+		else
+			region = 'intergrenic';
+		end
+
+		[tssind,closesttss] = min(abs(midpoint - chrknown(:,2)));
+		tssgenename = chrknown(tssind,1);
+
+		genelist = cell(1,3);
+		genedist = zeros(1,3);
+		[index,distance] = sort(abs(midpoint - chrknown(:,2)));
+		for closgene = 1:3
+			genelist{closgene} = chrknown(index(closgene),1);
+			genedist(closgene) = distance(closgene);
+			tommytssdistance = midpoint - chrknown(index(closgene),2);
+			tommytesdistance  = midpoint - chrknown(index(closgene),3);
+			if tommytssdistance < 0
+				genedist(closgene) = genedist(closgene)*-1;
+			else if tommytssdistance > 0 && tommytesdistance < 0
+				genedist(closgene) = 0;
+			else
+				continue
+			end
+		end
+
 		range = ceil(peak(2)/50):ceil(peak(3)/50);
 		chrom = peak(j,1) == peak(:,1);
 		[ind,val] = max(ip(chrom,range));
-		coordind = ceil(ind*50);
-		chromind = peak(j,1) == known(:,1);
-		[index,diff] = min(abs(ceil(known(chromind,2)/50) - coordind));
-		
-	end
-	for k = 1:length(IP)
-		if ~strcmp(IP{i},IP{k})
-			y  = load(sprintf('%s.mat',IP{k}));
-		else 
-			continue
-		end
 
+		for k = 1:length(IP)
+			if ~strcmp(IP{i},IP{k})
+				y  = load(sprintf('%s.mat',IP{k}));
+			else 
+				continue
+			end
+			maxenrich = max(y(chrom,range))
+		end
 	end
+
 end
