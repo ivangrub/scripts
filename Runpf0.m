5bb5e9b22f6ce537e2dbbe10f4ed1905fa33bb59
Ivan Grubisic <ivan.grub@gmail.com>
2013-04-22 15:31:43 -0700

    Add latest express simulations

--- Runpf0.m
+++ Runpf0.m
@@ -1,14 +1,14 @@
-dirs = {'sim_300bp_neigh1_wB' 'sim_300bp_wB' 'sim_900bp_wB' 'sim_1000bp_neigh1_wB' 'sim_3000bp_wB' 'sim_3000bp_neigh1_wB' 'sim_9000bp_wB' 'sim_unique'};
+dirs = {'ES_PI','ES_Pol2','ES_PI_150bp_neigh1_wB','ES_Pol2','ES_Pol2_150bp_neigh1_wB','ES_Pol2_neigh1_wB_samp','ES_TBP','ES_TBP_150bp_neigh1_wB'};
 
 for j = 1:length(dirs)
-    cd(sprintf('~/Desktop/express_sim/%s' ,dirs{j}));
+    cd(sprintf('/Volumes/Genomic1/IG_express/%s' ,dirs{j}));
     a = dir;
     [m,~] = size(a);
     for i = 1:m
         if strfind(a(i).name,'chip.') & strfind(a(i).name,'.bedgraph')
             fprintf(a(i).name)
             cd('~/Desktop')
-            pf0_fromexpresswiggle(sprintf('~/Desktop/express_sim/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
+            pf0_fromexpresswiggle(sprintf('/Volumes/Genomic1/IG_express/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
         end	
     end
 end
