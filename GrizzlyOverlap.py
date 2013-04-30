7b14c1af0693b94a5f3b3ce0a58ae042a76aac14
Ivan Grubisic <ivan.grub@gmail.com>
2013-04-30 13:30:19 -0700

    overlap filtered peaks

--- GrizzlyOverlap.py
+++ GrizzlyOverlap.py
@@ -2,15 +2,15 @@
 
 import os
 
-dir = os.listdir('/Users/ivang/Desktop')
+dir = os.listdir('/Volumes/Genomic1/IG_express/ES_Grizzly_Peaks')
 
 peaks = []
 for file in dir:
-	if 'broad_chip.' in file and '.peaks.bed' in file:
+	if 'broad_chip.' in file and 'filtered.peaks.bed' in file:
 		peaks.append(file)
 
 for peak in peaks:
 	for k in xrange(len(peaks)):
 		if peak != peaks[k]:
 			print peak, peaks[k]
-			os.system('intersectBed -a ~/Desktop/%s -b ~/Desktop/%s > ~/Desktop/overlapped_%s_%s.bed' % (peak,peaks[k],peak[:-4],peaks[k][:-4]))
+			os.system('intersectBed -a /Volumes/Genomic1/IG_express/ES_Grizzly_Peaks/%s -b /Volumes/Genomic1/IG_express/ES_Grizzly_Peaks/%s > /Volumes/Genomic1/IG_express/ES_Grizzly_Peaks/overlapped_%s_%s.bed' % (peak,peaks[k],peak[:-4],peaks[k][:-4]))
