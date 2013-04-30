71e86345713a9fab72587988058766a92938f1c5
Ivan Grubisic <grubisic@fixed-169-229-53-252.mcb.berkeley.edu>
2013-04-30 11:36:34 -0700

    CSEM specific

--- Runpf0.m
+++ Runpf0.m
@@ -1,14 +1,14 @@
-dirs = {'Sample_TY012','Sample_TY013','Sample_TY014','Sample_TY015'};
+dirs = {'CSEM'};
 
 for j = 1:length(dirs)
-    cd(sprintf('/Volumes/Teppei/%s' ,dirs{j}));
+    cd(sprintf('/Volumes/Genomic1/IG_express/%s' ,dirs{j}));
     a = dir;
     [m,~] = size(a);
     for i = 1:m
         if strfind(a(i).name,'chip.') & strfind(a(i).name,'.bedgraph')
             fprintf(a(i).name)
             cd('~/Desktop')
-            pf0_fromexpresswiggle(sprintf('/Volumes/Teppei/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
+            pf0_fromexpresswiggle(sprintf('/Volumes/Genomic1/IG_express/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
         end	
     end
 end
