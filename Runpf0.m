a50eba29a4c286339b8b626d92079d94cd37652c
Ivan Grubisic <grubisic@fixed-169-229-53-252.mcb.berkeley.edu>
2013-05-15 18:37:25 -0700

    new runpfo

--- Runpf0.m
+++ Runpf0.m
@@ -1,14 +1,14 @@
-dirs = {'ES_TAF1_unique' 'ES_TAF1_multi' 'ES_TAF7_unique' 'ES_TAF7_multi' 'MEF_TAF7_unique' 'MEF_TAF7_multi'};
+dirs = {'CSEM'};
 
 for j = 1:length(dirs)
-    cd(sprintf('/Volumes/Teppei/%s' ,dirs{j}));
+    cd(sprintf('/Volumes/Genomic1/IG_express/%s' ,dirs{j}));
     a = dir;
     [m,~] = size(a);
     for i = 1:m
         if strfind(a(i).name,'chip.') & strfind(a(i).name,'.bedgraph')
             fprintf(a(i).name)
             cd('~/Desktop')
-            pf0_fromexpresswiggle(sprintf('/Volumes/Teppei/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
+            pf0_fromexpresswiggle(sprintf('/Volumes/Genomic1/IG_express/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
         end	
     end
 end
