69cccc85d860bc2040eaa8ecfeffb2d16556841e
Ivan Grubisic <ivan.grub@gmail.com>
2013-05-16 17:19:38 -0700

    for MACs

---
+++ MultiWig2Wig.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+x = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chrX','chrY','chrM']
+
+outfile = open('Combinedwiggle.wig','w')
+
+j = 0
+for i in x:
+	wig = open('%s.wig' % i)
+	for line in wig
+		if j == 0:
+			outfile.write(line)
+			j += 1
+			continue
+
+		outfile.write(line)
+
+	wig.close()
+
+outfile.close()
