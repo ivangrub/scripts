59c8c895f3d0c762e61b68de4ddcee49bc2c119f
Ivan Grubisic <grubisic@fixed-169-229-53-252.mcb.berkeley.edu>
2013-07-25 17:54:46 -0700

    Make csem specific

--- PrintAllProbs.py
+++ PrintAllProbs.py
@@ -3,8 +3,11 @@
 import pysam as pys
 
 out = open('ReadProbability.txt','w')
-reads = pys.Samfile('hits.1.prob.bam','rb')
+reads = pys.Samfile('CSEM_Fly_Pol2.k500.sim_hisrepeat.bam','rb')
 
 for r in reads:
-	prob = r.opt('XP')
+	try:
+		prob = r.opt('ZW')
+	except KeyError:
+		continue
 	out.write('%s\t%s\n' % (r.qname,prob))
