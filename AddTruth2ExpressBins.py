fe53455100cb7dc850e78094a1e774ce6003614d
Ivan Grubisic <ivan.grub@gmail.com>
2013-07-25 18:42:11 -0700

    remove header line

--- AddTruth2ExpressBins.py
+++ AddTruth2ExpressBins.py
@@ -6,12 +6,11 @@ out = open('CoordwTruth.txt','w')
 i = 0
 for line in results:
 	if i == 0:
-		out.write('Chr\tCoord1\tCoord2\tSolvable\n')
 		i += 1
 		continue
 	s = line.strip().split()
 	bin = s[1].split('!')
-	out.write('%s\t%s\t%s\t%s\n' % (bin[1],bin[2],bin[3],s[13])
+	out.write('%s\t%s\t%s\t%s\n' % (bin[1],bin[2],bin[3],s[13]))
 
 out.close()
 results.close()
