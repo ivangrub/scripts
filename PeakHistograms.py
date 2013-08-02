fb78c7537fe50afab232bb84bf15dd206af35dde
Ivan Grubisic <ivan.grub@gmail.com>
2013-08-01 18:55:02 -0700

    Update numpy code to make it faster

--- PeakHistograms.py
+++ PeakHistograms.py
@@ -3,6 +3,7 @@
 import pysam as pys
 import numpy as np
 import argparse
+#import yappi
 
 def GetLikelihood(seq,ind):
 	frac_str = seq.opt('XP')
@@ -12,9 +13,9 @@ def GetLikelihood(seq,ind):
 def FindOverlap(seq,coords,chromo,ref):
 	chrom = ref[seq.tid]
 	st = seq.pos
-	a = chrom == chromo
-	c = st >= coords[:,0]
-	d = st <= coords[:,1]
+	a = np.core.defchararray.equal(chrom,chromo)
+	c = np.greater_equal(st,coords[:,0])
+	d = np.less_equal(st,coords[:,1])
 	cboth = np.logical_and(c,d)
 	try:
 		return np.where(np.logical_and(a,cboth))[0][0]
@@ -41,22 +42,26 @@ for line in peak:
 	chr = np.append(chr,[[s[0]]])
 	coord = np.append(coord,[float(s[1]),float(s[2])])
 
+peak.close()
 x = len(coord)
 crd = np.reshape(coord,(x/2,2))
 hist = np.zeros((x/2,100))
-
+#yappi.start()
 i = 0
 for read in bam:
 	i += 1
-	
+	if (i % 1000000 == 0):
+		print 'Processed %d Alignments' % i
+
 	index = FindOverlap(read,crd,chr,refdict)
+	
 	if (index == False):
 		continue
 	else:
 		GetLikelihood(read,index)
-
-	if (i % 1000000 == 0):
-		print 'Processed %d Reads' % i
+		
+	#yappi.print_stats()
+	
 
 
 print 'Printing Text file'
@@ -64,5 +69,5 @@ for i in xrange(np.shape(hist)[0]):
 	hist_out.write('%s\t%s\n' % (name[i],"\t".join(map(str,hist[i]))))
 
 hist_out.close()
-peak.close()
+
 bam.close()
