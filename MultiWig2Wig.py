061f0aee6d87a85e9d17db3322a3a91bdb37babb
Ivan Grubisic <ivan.grub@gmail.com>
2013-09-10 12:23:35 -0700

    Add options

--- MultiWig2Wig.py
+++ MultiWig2Wig.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a genome fasta file that will be used to create the bowtie index and used as an input eXpress.')
+parser.add_argument('-p',help = 'Prefix of the wiggle files',default = None)
+parser.add_argument('-o',help = 'Prefix of combined wiggle file',default = None)
+args = parser.parse_args()
+
+if args.p == None:
+	print 'Provide a prefix file'
+
 x = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chrX','chrY','chrM']
 
-outfile = open('Combinedwiggle.wig','w')
+outfile = open('%s.wig' % args.o,'w')
 
 j = 0
 for i in x:
 	k = 0
+	prefix = args.p+'_'+i
 	wig = open('%s.wig' % i)
 	for line in wig:
 		if  j== 0 and k == 0:
--- Runpf0.m
+++ Runpf0.m
@@ -1,17 +1,17 @@
-dirs = {'CSEM'};
+dirs = {'Desktop'};
 
 for j = 1:length(dirs)
-    cd(sprintf('/Volumes/Genomic1/IG_express/%s' ,dirs{j}));
+    cd(sprintf('~/%s' ,dirs{j}));
     a = dir;
     [m,~] = size(a);
     for i = 1:m
-        if strfind(a(i).name,'chip.') & strfind(a(i).name,'.bedgraph')
+        if strfind(a(i).name,'chip.ES_TBP') & strfind(a(i).name,'.bedgraph')
             fprintf(a(i).name)
             if ~strcmp(a(i).name,'chip.CSEM_ES_Pol2.k100.bedgraph')
                 continue
             end
             cd('~/Desktop')
-            pf0_fromexpresswiggle(sprintf('/Volumes/Genomic1/IG_express/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
+            pf0_fromexpresswiggle(sprintf('~/%s/%s',dirs{j},a(i).name),50,250,'~/Desktop/genome_mm9.mat')
         end	
     end
 end
